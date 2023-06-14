9927cae5b29eee9f9f5c657f50dbea33623c05c2
OCTAVIO ARFA TENENBERG <47292048@ort>
2023-06-14 08:53:08 -0300

    imagen pais

--- src/App.js
+++ src/App.js
@@ -7,10 +7,12 @@ function App() {
   const [paises, setPaises] = useState([]);
   const [paisRandom, setPaisRandom] = useState({});
   const [puntos, setPuntos] = useState(0);
+  const pResultado = document.getElementById('resultado');
+
   useEffect(() => {
     axios.get('https://countriesnow.space/api/v0.1/countries/flag/images')
-      .then(function (response) {
-        setPaises(
+      .then(async function (response) {
+      setPaises(
           [...response.data.data]
         )
       })
@@ -28,17 +30,32 @@ function App() {
   useEffect(() => {
     console.log(paises);
     console.log(paisRandom);
+    //document.getElementById('imgPais').src=paisRandom.flag;
   }, [paisRandom])
+  useEffect(() => {
+    setTimeout(() => {
+      // document.getElementById('timer').innerText=''
+      console.log('a');
+    }, 1000)
+  })
   return (
     <div className="App">
+      { paisRandom ? <img id="imgPais" src={paisRandom.flag} /> : <div></div> } 
       <form onSubmit={(e) => {
         e.preventDefault();
         console.log(e.target.elements["pais"].value);
         if(e.target.elements["pais"].value == paisRandom.name) {
           setPuntos(puntos+10);
+          pResultado.style.color='green';
+          pResultado.innerText='Bien';
           console.log("bien");
+          setPaisRandom(
+            paises[Math.round(Math.random()*220)]
+          )
         } else {
           setPuntos(puntos-1);
+          pResultado.style.color='red';
+          pResultado.innerText='Mal';
           console.log("mal");
         }
       }}>
@@ -46,6 +63,8 @@ function App() {
         <input type={"submit"} value={"comprobar"}></input> 
       </form>
       <p>Puntos: {puntos}</p>
+      <p id='resultado'></p>
+      <p id="timer"></p>
     </div>
   );
 }
